0145413f9b5b40ec02f1ba326ef35f87cd4d386c
andrew godeke <legodeke@hotmail.com>
2024-04-16 21:16:50 -0400

    models all green

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,19 @@
 class Movie < ApplicationRecord
   # Validations
   validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 
   # Associations
-  has_many :bookmarks, dependent: :destroy
-  has_many :lists, through: :bookmarks
+  has_many :bookmarks
+
+  before_destroy :check_for_bookmarks
+
+  private
+
+  def check_for_bookmarks
+    if bookmarks.exists?
+      errors.add('cannot be deleted because it has associated bookmarks')
+      return false
+    end
+  end
 end
